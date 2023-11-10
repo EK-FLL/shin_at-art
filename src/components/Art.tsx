8cc35eae72fbe422ed95a40e1181e78ec46348b5
sas-news <sasshinbun0655@gmail.com>
2023-11-10 20:08:51 +0900

    型指定

--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import styles from "@/styles/Art.module.scss";
-
-const Art = ({ id }) => {
+type Prop = {
+  id: string;
+};
+const Art = ({ id }: Prop) => {
   const [comments, setComments] = useState([
     { text: "コメント1", x: 80, y: 90 },
     { text: "コメント2", x: 50, y: 10 },
@@ -16,19 +18,19 @@ const Art = ({ id }) => {
       <div className={styles.size} style={{ position: "relative" }}>
         <Image className={styles.image} src={imageSrc} alt="アートです" fill />
         {comments.map((comment, index) => (
-          <div
-            key={index}
-            style={{
-              position: "absolute",
-              left: `calc(${comment.x}% - 20px)`,
-              top: `calc(${comment.y}% - 20px)`,
-              backgroundColor: "rgba(255, 255, 255, 0.7)",
-              padding: "5px",
-              borderRadius: "5px",
-            }}
-          >
-            {comment.text}
-          </div>
+          <>
+            <div
+              className={styles.art_comment}
+              key={index}
+              style={{
+                position: "absolute",
+                left: `calc(${comment.x}% - 0px)`,
+                top: `calc(${comment.y}% - 0px)`,
+              }}
+            >
+              {comment.text}
+            </div>
+          </>
         ))}
       </div>
     </>
