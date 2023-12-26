9dc5bff161fbdaf4b5673b818125047c538962f6
sasnews <sasshinbun0655@gmail.com>
2023-12-27 07:09:57 +0900

    全体アクセス可能

--- app/[author]/page.tsx
+++ app/[author]/page.tsx
@@ -4,22 +4,50 @@ import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { db } from "@/app/_globals/firebase";
 import { useEffect, useState } from "react";
 import { type } from "os";
-const getAuthor = async (author: string) => {
-  const artRef = collection(db, "artworks", author, "arts");
-  const artSnap = await getDocs(artRef);
-  console.log(artSnap);
-  artSnap.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-  });
-  const authorRef = doc(db, "authors", author);
-  const authorSnap = await getDoc(authorRef);
-  if (authorSnap.exists()) {
-    return authorSnap.data();
-  } else {
-    return "データが見つかりません。";
-  }
+import Link from "next/link";
+
+type Art = {
+  id: string;
+  name: string;
 };
 const Home = () => {
+  const [arts, setArts] = useState<Art[]>([]);
+  const getAuthors = async () => {
+    try {
+      let artsData: Art[] = [];
+      const querySnapshot = await getDocs(
+        collection(db, "authors", author, "arts")
+      );
+      querySnapshot.forEach((doc) => {
+        const art: Art = {
+          id: doc.id,
+          name: (doc.data() as Art).name,
+        };
+        artsData.push(art);
+      });
+      setArts(artsData);
+    } catch (error) {
+      console.error("エラー:", error);
+    }
+  };
+  useEffect(() => {
+    getAuthors();
+  }, []);
+  const getAuthor = async (author: string) => {
+    const artRef = collection(db, "artworks", author, "arts");
+    const artSnap = await getDocs(artRef);
+    console.log(artSnap);
+    artSnap.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+    const authorRef = doc(db, "authors", author);
+    const authorSnap = await getDoc(authorRef);
+    if (authorSnap.exists()) {
+      return authorSnap.data();
+    } else {
+      return "データが見つかりません。";
+    }
+  };
   const { author } = useParams() as { author: string };
   const [authorName, setAuthorName] = useState();
   useEffect(() => {
@@ -40,6 +68,12 @@ const Home = () => {
   return (
     <>
       <h1>{authorName}のプロフ</h1>
+      {arts.map((art, index) => (
+        <Link key={index} href={`${author}/${art.id}`}>
+          {art.name}
+          <br />
+        </Link>
+      ))}
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+"use client";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "./_globals/firebase";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+type Author = {
+  id: string;
+  name: string;
+};
 const Home = () => {
+  const [authors, setAuthors] = useState<Author[]>([]);
+  const getAuthors = async () => {
+    try {
+      let authorsData: Author[] = [];
+      const querySnapshot = await getDocs(collection(db, "authors"));
+      querySnapshot.forEach((doc) => {
+        const author: Author = {
+          id: doc.id,
+          name: (doc.data() as Author).name,
+        };
+        authorsData.push(author);
+      });
+      setAuthors(authorsData);
+    } catch (error) {
+      console.error("エラー:", error);
+    }
+  };
+  useEffect(() => {
+    getAuthors();
+  }, []);
   return (
     <>
       <h1>Home Next.js</h1>
+
+      {authors.map((author, index) => (
+        <Link key={index} href={author.id}>
+          {author.name}
+          <br />
+        </Link>
+      ))}
     </>
   );
 };
